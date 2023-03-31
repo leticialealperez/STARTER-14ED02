dd5142d67ccf83edb2db2e78e40315ec65c7bdac
Leticia Leal <leticialealp@hotmail.com>
2023-03-30 22:49:50 -0300

    first commit

---
+++ aula02-30.03/index.js
@@ -0,0 +1,52 @@
+// comentario de linha
+
+/*   
+    comentario de bloco 
+    estar como um comentario 
+*/
+// alert("Hello World") // são utilizados para quando precisamos mostrar alguma mensagem ao usuario
+
+// confirm("Deseja realmente excluir esse item?") 
+
+// prompt("Digite seu nome: ")
+
+
+// COMO ARMAZENAR DADOS NO JS?
+// variaveis
+
+// declarar uma variavel
+// LET => É MUTÁVEL
+
+
+// CONST => NÃO É MUTÁVEL, SEMPRE VAI TER O MESMO VALOR
+const pi = 3.14
+
+
+// TIPOS DE DADOS
+
+// texto => STRINGS
+let nomeUsuario = "Joao da Silva"
+let paragrafo = 'Era uma vez alguma coisa, "coisado"'
+
+// numero => number
+let idade = 27
+
+let celsius = 30.5
+celsius = 40.5 // atribuição de um novo valor
+
+
+// booleano => true ou false
+let maiorIdade = false
+
+
+// let confirmou = confirm("Deseja realmente excluir esse item?") 
+
+// alert(confirmou)
+
+// let email = prompt("Digite seu e-mail: ")
+
+// alert(email)
+
+
+
+
