eadee787f5f4122c95beccce1832d465c302fb3b
Leticia Leal <leticialealp@hotmail.com>
2023-04-04 22:34:46 -0300

    exemplo de condicional composta

--- aula03-04.04/js/condicionais-compostas.js
+++ aula03-04.04/js/condicionais-compostas.js
@@ -1,5 +1,22 @@
 // IF ELSE IF
+let condicao1 = false;
+let condicao2 = false;
+let condicao3 = true;
 
+// QUAL LOG SERÁ MOSTRADO?
+
+if(condicao1) {
+    console.log("Condição 1")
+
+} else if(condicao2) {
+    console.log("Condição 2")
+
+} else if(condicao3) {
+    console.log("Condição 3")
+
+} else {
+    console.log("Nenhuma condição acima é TRUE")
+}
 
 
 
