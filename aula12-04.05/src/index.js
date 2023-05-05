680e6c9b23f2eed87bad1498623fb93f2c87feeb
Leticia Leal <leticialealp@hotmail.com>
2023-05-04 22:44:12 -0300

    adicionado o create de user

--- aula12-04.05/src/index.js
+++ aula12-04.05/src/index.js
@@ -18,26 +18,7 @@ app.get('/', (request, response) => {
 })
 
 // DATABASE
-const listaUsuarios = [
-    {
-        id: 1,
-        email: 'joao@teste.com',
-        password: '123',
-        transactions: []
-    },
-    {
-        id: 2,
-        email: 'maria@teste.com',
-        password: '1234',
-        transactions: []
-    },
-    {
-        id: 3,
-        email: 'joaofelipe@teste.com',
-        password: '12345',
-        transactions: []
-    }
-]
+const listaUsuarios = []
 
 // -------------------------------------------------------------------------------------------
 // ROTAS
@@ -46,7 +27,7 @@ const listaUsuarios = [
 
 // GET - LISTAR TODOS OS USUÁRIOS
 // QUERY PARAMS - /users?email="Joao"&idade="18" - filtros
-// PATH PARAMS - /users/1 - buscar UM, atualizar, deletar
+
 app.get('/users', (request, response) => {
 
     // QUERY = SEMPRE UM OBJETO
@@ -87,8 +68,84 @@ app.get('/users', (request, response) => {
         dados: listaUsuarios,
         mensagem: 'Dados de usuários buscados com sucesso!'
     })
-})
+});
 
 // GET - LISTAR UM USUARIO POR ID
+// PATH PARAMS - /users/1 - buscar UM, atualizar, deletar
+app.get('/users/:id', (request, response) => {
+    // PARAMETRO DE REQUISIÇÃO É UM OBJETO { id: 17 }
+    const params = request.params
+    console.log(params)
+
+    // == valor  === valor e tipo
+    const usuarioEncontrado = listaUsuarios.find((user) => user.id == params.id)
+
+    if (!usuarioEncontrado) {
+        return response.status(404).json({
+            sucesso: false,
+            dado: null,
+            mensagem: 'Usuário não encontrado pelo ID informado'
+        })
+    }
+
+    return response.status(200).json({
+        sucesso: true,
+        dados: usuarioEncontrado,
+        mensagem: 'Usuário encontrado com sucesso!'
+    })
+})
+
+
+// POST - inserir/cadastrar/criar - CREATE
+// body - É UM PACOTE EM FORMATO JSON COM OS DADOS NECESSARIOS PARA CADASTRAR UM USUARIO
+app.post('/users', (request, response) => {
+    const dados = request.body
+
+    if (!dados.email || !dados.email.includes('@') || !dados.email.includes('.com')) {
+        return response.status(400).json({
+            sucesso: false,
+            dados: null,
+            mensagem: "É obrigatório informar um e-mail válido para cadastro do usuário"
+        })
+    }
+
+    if (!dados.password || dados.password.length < 6) {
+        return response.status(400).json({
+            sucesso: false,
+            dados: null,
+            mensagem: "É obrigatório informar a senha para cadastro do usuário com no mínimo 6 caracteres"
+        })
+    }
+
+    // new Date()
+    // .getTime() - retorna a quantidade de milessegundos desde 1970 1 de janeiro até o agora
+
+    const novoUsuario = {
+        id: new Date().getTime(),
+        email: dados.email,
+        password: dados.password,
+        transactions: []
+    }
+
+    const existe = listaUsuarios.some((user) => user.email === novoUsuario.email)
+
+    if (existe) {
+        return response.status(400).json({
+            sucesso: false,
+            dados: null,
+            mensagem: "Outro usuário já está cadastrado com este e-mail."
+        })
+    }
+
+    // adicionar o novo usuario na lista
+    listaUsuarios.push(novoUsuario)
+
+    return response.status(201).json({
+        sucesso: true,
+        dados: novoUsuario,
+        mensagem: "Usuário cadastrado com sucesso!"
+    })
+
+})
 
 
