ea8f5bf029ae3397b7ff8c6a73d93a8358e6fc7b
Leticia Leal <leticialealp@hotmail.com>
2023-07-11 18:46:52 -0300

    start aula

--- BLOCO-INTERMEDIARIO/ORIENTACAO-A-OBJETOS/aula03-11.07.2023/src/index.ts
+++ BLOCO-INTERMEDIARIO/ORIENTACAO-A-OBJETOS/aula03-11.07.2023/src/index.ts
@@ -1,4 +1,26 @@
 import prompt from 'prompt-sync';
 const teclado = prompt();
 
-console.log(teclado('Digite algo: '));
+/*
+
+ O QUE É UMA CLASSE?
+
+ - É a abstração do que há em comum (em termos de caracteristicas e comportamentos) a um conjunto de objetos.
+ É um molde/esqueleto para a criação de objetos.
+
+ Ex: Empresa, Aluno, Imovel
+
+ No final será um tipo de dado personalizado à nossa aplicação/software bem como string, number e boolean por exemplo
+
+*/
+
+/*
+
+    MAS ENTÃO, O QUE É UM OBJETO?
+
+    Um objeto é uma instância de uma classe. Um dado gerado a partir de um molde (uma classe).
+    Terá a combinação da estrutura de dados (atributos) e o comportamento (métodos) da classe que o gerou.
+
+    Ex: Um aluno, Uma empresa, Um imovel
+
+*/
