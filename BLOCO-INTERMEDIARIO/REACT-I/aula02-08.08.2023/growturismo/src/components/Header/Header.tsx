9e79ef0c1dc6393d60b4f9a368204eda431c2a1a
Anderson Wilmsen <ander.wilmsen98@gmail.com>
2023-08-09 17:46:11 -0300

    feat: test commit

--- BLOCO-INTERMEDIARIO/REACT-I/aula02-08.08.2023/growturismo/src/components/Header/Header.tsx
+++ BLOCO-INTERMEDIARIO/REACT-I/aula02-08.08.2023/growturismo/src/components/Header/Header.tsx
@@ -1,29 +1,29 @@
-import FlexContainerStyled from '../shared/FlexContainerStyled';
-import TittleStyled from '../shared/TittleStyled';
-import HeaderStyled from './HeaderStyled';
-import NavigationStyled from './NavigationStyled';
+import FlexContainerStyled from "../shared/FlexContainerStyled";
+import TittleStyled from "../shared/TittleStyled";
+import HeaderStyled from "./HeaderStyled";
+import NavigationStyled from "./NavigationStyled";
 
 function Header() {
-	return (
-		<HeaderStyled>
-			<FlexContainerStyled modo='header'>
-				<TittleStyled tamanho='md'>Growtravel</TittleStyled>
-				<NavigationStyled>
-					<ul>
-						<li>
-							<a href=''>Quem Somos</a>
-						</li>
-						<li>
-							<a href=''>Serviços</a>
-						</li>
-						<li>
-							<a href=''>Planos</a>
-						</li>
-					</ul>
-				</NavigationStyled>
-			</FlexContainerStyled>
-		</HeaderStyled>
-	);
+  return (
+    <HeaderStyled>
+      <FlexContainerStyled modo="header">
+        <TittleStyled tamanho="md">GrowTravel</TittleStyled>
+        <NavigationStyled>
+          <ul>
+            <li>
+              <a href="">Quem Somos</a>
+            </li>
+            <li>
+              <a href="">Serviços</a>
+            </li>
+            <li>
+              <a href="">Planos</a>
+            </li>
+          </ul>
+        </NavigationStyled>
+      </FlexContainerStyled>
+    </HeaderStyled>
+  );
 }
 
 export default Header;
