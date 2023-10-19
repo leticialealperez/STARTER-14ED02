87d721d9661efc38df89e6969932f2209e692c3e
Anderson Wilmsen <ander.wilmsen98@gmail.com>
2023-10-18 22:44:02 -0300

    aula 18.10
    Co-authored-by: Eduarda Löbell da Silva <eduardaLobell@users.noreply.github.com>
    Co-authored-by: giehl.felipe@gmail.com <giehl.felipe@gmail.com>

--- BLOCO-INTERMEDIARIO/BANCO-DE-DADOS-II/api-controle-academico/src/services/avaliacao.service.ts
+++ BLOCO-INTERMEDIARIO/BANCO-DE-DADOS-II/api-controle-academico/src/services/avaliacao.service.ts
@@ -1,114 +1,121 @@
-import { Avaliacao as AvaliacaoPrisma } from '@prisma/client';
-import repository from '../database/prisma.connection';
+import { Avaliacao as AvaliacaoPrisma } from "@prisma/client";
+import repository from "../database/prisma.connection";
 import {
-	AtualizarAvaliacaoDTO,
-	CadastrarAvaliacaoDTO,
-	DeletarAvaliacaoDTO,
-	ListarAvaliacaoPorIdDTO,
-	ResponseDTO,
-} from '../dtos';
-import { Avaliacao } from '../models';
+  AtualizarAvaliacaoDTO,
+  CadastrarAvaliacaoDTO,
+  DeletarAvaliacaoDTO,
+  ListarAvaliacaoPorIdDTO,
+  ResponseDTO,
+} from "../dtos";
+import { Avaliacao } from "../models";
 
 export class AvaliacaoService {
-	public async criar(dado: CadastrarAvaliacaoDTO): Promise<ResponseDTO> {
-		const novaAvaliacao = await repository.avaliacao.create({
-			data: {
-				modulo: dado.modulo,
-				nota: dado.nota,
-				idAluno: dado.idAluno,
-			},
-		});
+  public async criar(dado: CadastrarAvaliacaoDTO): Promise<ResponseDTO> {
+    const novaAvaliacao = await repository.avaliacao.create({
+      data: {
+        modulo: dado.modulo,
+        nota: dado.nota,
+        idAluno: dado.idAluno,
+      },
+    });
 
-		return {
-			code: 201,
-			ok: true,
-			mensagem: 'Avaliação cadastrada com sucesso!',
-			dados: this.mapToModel(novaAvaliacao),
-		};
-	}
+    return {
+      code: 201,
+      ok: true,
+      mensagem: "Avaliação cadastrada com sucesso!",
+      dados: this.mapToModel(novaAvaliacao),
+    };
+  }
 
-	public async listarTodos(idAluno: string): Promise<ResponseDTO> {
-		const avaliacoes = await repository.avaliacao.findMany({
-			where: { idAluno },
-		});
+  public async listarTodos(idAluno: string): Promise<ResponseDTO> {
+    const avaliacoes = await repository.avaliacao.findMany({
+      where: { idAluno },
+    });
 
-		if (!avaliacoes.length) {
-			return {
-				code: 404,
-				ok: false,
-				mensagem: 'Não foram encontradas avaliações para este aluno.',
-			};
-		}
+    if (!avaliacoes.length) {
+      return {
+        code: 404,
+        ok: false,
+        mensagem: "Não foram encontradas avaliações para este aluno.",
+      };
+    }
 
-		return {
-			code: 200,
-			ok: true,
-			mensagem: 'Avaliações listadas com sucesso',
-			dados: avaliacoes.map((a) => this.mapToModel(a)),
-		};
-	}
+    return {
+      code: 200,
+      ok: true,
+      mensagem: "Avaliações listadas com sucesso",
+      dados: avaliacoes.map((a) => this.mapToModel(a)),
+    };
+  }
 
-	public async listarPorID(dados: ListarAvaliacaoPorIdDTO): Promise<ResponseDTO> {
-		const avaliacaoEncontrada = await repository.avaliacao.findUnique({
-			where: {
-				id: dados.idAvaliacao,
-				idAluno: dados.idAluno,
-			},
-		});
+  public async listarPorID(
+    dados: ListarAvaliacaoPorIdDTO
+  ): Promise<ResponseDTO> {
+    const avaliacaoEncontrada = await repository.avaliacao.findUnique({
+      where: {
+        id: dados.idAvaliacao,
+        idAluno: dados.idAluno,
+      },
+    });
 
-		if (!avaliacaoEncontrada) {
-			return {
-				code: 404,
-				ok: false,
-				mensagem: 'Avaliação não encontrada',
-			};
-		}
+    if (!avaliacaoEncontrada) {
+      return {
+        code: 404,
+        ok: false,
+        mensagem: "Avaliação não encontrada",
+      };
+    }
 
-		return {
-			code: 200,
-			ok: true,
-			mensagem: 'Avaliação encontrada com sucesso',
-			dados: this.mapToModel(avaliacaoEncontrada),
-		};
-	}
+    return {
+      code: 200,
+      ok: true,
+      mensagem: "Avaliação encontrada com sucesso",
+      dados: this.mapToModel(avaliacaoEncontrada),
+    };
+  }
 
-	public async atualizar(dados: AtualizarAvaliacaoDTO): Promise<ResponseDTO> {
-		const avaliacaoAtualizada = await repository.avaliacao.update({
-			where: {
-				id: dados.idAvaliacao,
-				idAluno: dados.idAluno,
-			},
-			data: {
-				modulo: dados.modulo,
-				nota: dados.nota,
-			},
-		});
+  public async atualizar(dados: AtualizarAvaliacaoDTO): Promise<ResponseDTO> {
+    const avaliacaoAtualizada = await repository.avaliacao.update({
+      where: {
+        id: dados.idAvaliacao,
+        idAluno: dados.idAluno,
+      },
+      data: {
+        modulo: dados.modulo,
+        nota: dados.nota,
+      },
+    });
 
-		return {
-			code: 200,
-			ok: true,
-			mensagem: 'Avaliação atualizada com sucesso',
-			dados: this.mapToModel(avaliacaoAtualizada),
-		};
-	}
+    return {
+      code: 200,
+      ok: true,
+      mensagem: "Avaliação atualizada com sucesso",
+      dados: this.mapToModel(avaliacaoAtualizada),
+    };
+  }
 
-	public async deletar(dados: DeletarAvaliacaoDTO): Promise<ResponseDTO> {
-		const avaliacaoExcluida = await repository.avaliacao.delete({
-			where: {
-				id: dados.idAvaliacao,
-				idAluno: dados.idAluno,
-			},
-		});
+  public async deletar(dados: DeletarAvaliacaoDTO): Promise<ResponseDTO> {
+    const avaliacaoExcluida = await repository.avaliacao.delete({
+      where: {
+        id: dados.idAvaliacao,
+        idAluno: dados.idAluno,
+      },
+    });
 
-		return {
-			code: 200,
-			ok: true,
-			mensagem: 'Avaliação excluida com sucesso',
-			dados: this.mapToModel(avaliacaoExcluida),
-		};
-	}
+    return {
+      code: 200,
+      ok: true,
+      mensagem: "Avaliação excluida com sucesso",
+      dados: this.mapToModel(avaliacaoExcluida),
+    };
+  }
 
-	private mapToModel(avaliacao: AvaliacaoPrisma): Avaliacao {
-		return new Avaliacao(avaliacao.id, avaliacao.idAluno, avaliacao.modulo, Number(avaliacao.nota));
-	}
+  private mapToModel(avaliacao: AvaliacaoPrisma): Avaliacao {
+    return new Avaliacao(
+      avaliacao.id,
+      avaliacao.idAluno,
+      avaliacao.modulo,
+      Number(avaliacao.nota)
+    );
+  }
 }
--- BLOCO-INTERMEDIARIO/REACT-II/app-controle-academico/src/pages/Home.tsx
+++ BLOCO-INTERMEDIARIO/REACT-II/app-controle-academico/src/pages/Home.tsx
@@ -1,35 +1,198 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  TextField,
+} from "@mui/material";
+import Typography from "@mui/material/Typography";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Aluno, TabelaAlunos } from "../components/TabelaAlunos";
-import { listarAlunos } from "../services/aluno.service";
+import {
+  AtualizarAvaliacao,
+  atualizarAvaliacao,
+  cadastrarAvaliacao,
+  deletarAvaliacao,
+  listarTodasAvaliacoes,
+} from "../services/avaliacao.service";
+
+interface Avaliaco {
+  id: string;
+  idAluno: string;
+  nota: number;
+  modulo: string;
+}
 
 function Home() {
   const navigate = useNavigate();
   const token = JSON.parse(localStorage.getItem("token") ?? "false");
-  const [listaAlunos, setListaAlunos] = useState<Aluno[]>([]);
 
+  const [listaAvaliacoes, setListaAvaliacoes] = useState<Avaliaco[]>([]);
+
+  // CONTROLA SE TEM O TOKEN NO LOCAL STORAGE OU NAO
   useEffect(() => {
     if (!token) {
       return navigate("/");
     }
   }, []);
 
-  useEffect(() => {
-    async function buscarListaAlunos() {
-      const resposta = await listarAlunos(token);
-      setListaAlunos(resposta.dados);
-    }
+  // BUSCA TODAS AS AVALIACOES DO USUARIO LOGADO
+  async function buscarAvaliacoes() {
+    const resposta = await listarTodasAvaliacoes(token);
 
-    buscarListaAlunos();
+    setListaAvaliacoes(resposta.dados);
+
+    console.log("Chamou a função buscar todas avaliações");
+  }
+
+  useEffect(() => {
+    buscarAvaliacoes();
   }, []);
 
-  return (
-    <>
-      <h1>Bem-vindo</h1>
-      <h2>Listagem de Alunos</h2>
+  // CADASTRAR UMA NOVA AVALIAÇÃO
+  async function handleSubmit(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
 
-      <TabelaAlunos listaAlunos={listaAlunos} />
-    </>
+    const dados = {
+      nota: evento.currentTarget.nota.value,
+      modulo: evento.currentTarget["modulo"].value,
+    };
+
+    const resposta = await cadastrarAvaliacao(token, dados);
+
+    if (!resposta.ok) {
+      alert(resposta.mensagem);
+      return;
+    }
+
+    alert(resposta.mensagem);
+
+    evento.currentTarget.modulo.value = "";
+    evento.currentTarget.nota.value = null;
+  }
+
+  // EDITAR
+  async function editar(id: string) {
+    const modulo = prompt(`Digite o novo módulo: `);
+    const nota = prompt(`Digite a nova nota: `);
+
+    const notaConvertido = Number(nota);
+
+    const dados: AtualizarAvaliacao = {
+      idAvaliacao: id,
+      modulo: modulo ? modulo : undefined,
+      nota: notaConvertido ? notaConvertido : undefined,
+    };
+
+    if (!modulo && !notaConvertido) {
+      return;
+    }
+
+    const resposta = await atualizarAvaliacao(token, dados);
+
+    alert(resposta.mensagem);
+    buscarAvaliacoes();
+  }
+
+  // DELETAR
+  async function deletar(id: string) {
+    const confirmado = confirm(
+      `Você deseja realmente excluir a avaliação de ID: ${id}? `
+    );
+
+    if (!confirmado) {
+      return;
+    }
+
+    const resposta = await deletarAvaliacao(token, id);
+    alert(resposta.mensagem);
+
+    // BUSCA A LISTA ATUALIZADA LA DO BACKEND
+    buscarAvaliacoes();
+  }
+
+  return (
+    <Box>
+      <Grid item xs={12}>
+        <Typography variant="h4" color="initial">
+          Formulário de Avaliações
+        </Typography>
+      </Grid>
+
+      <form id="form-cadastro-avaliacao" onSubmit={handleSubmit}>
+        <Grid container spacing={1} sx={{ mb: 4 }}>
+          <Grid item xs={3}>
+            <TextField
+              id="modulo"
+              label="Modulo"
+              variant="outlined"
+              fullWidth
+              sx={{ mt: 1 }}
+            />
+          </Grid>
+          <Grid item xs={1}>
+            <TextField
+              id="nota"
+              label="Nota"
+              variant="outlined"
+              type="number"
+              fullWidth
+              sx={{ mt: 1 }}
+            />
+          </Grid>
+          <Grid item xs={1}>
+            <Button
+              variant="contained"
+              type="submit"
+              fullWidth
+              sx={{ mt: 1, py: 2 }}
+            >
+              Enviar
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+
+      <Grid container spacing={1}>
+        {listaAvaliacoes.map((avaliacao) => (
+          <Grid item xs={12} sm={6} md={4} lg={2} key={avaliacao.id}>
+            <Card sx={{ background: "#eaeaea" }}>
+              <CardContent>
+                <Typography
+                  sx={{ fontSize: 14 }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  {avaliacao.modulo}
+                </Typography>
+
+                <Typography variant="body2">{avaliacao.nota}</Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  size="small"
+                  variant="contained"
+                  color="info"
+                  onClick={() => editar(avaliacao.id)}
+                >
+                  Editar
+                </Button>
+                <Button
+                  size="small"
+                  variant="contained"
+                  color="error"
+                  onClick={() => deletar(avaliacao.id)}
+                >
+                  Apagar
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 }
 
